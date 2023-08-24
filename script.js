9f6c2de830ff5937f660d698405bd14e71e712b3
dthcs <dthcsc@gmail.com>
2023-08-24 09:06:19 +0900

    d

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var audio = new Audio('assets/sentmessage.mp3');
-var contactString = "<div class='social'> <a target='_blank' href='tel:+82-010-xxxx-xxxx'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>010xxxxxxxx</label></div> </a> <a href='mailto:doanhuyendth@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/dthcs'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/dth96_'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/dthcsc/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
+var contactString = "Feel free to use contact information below to get in touch with me <br><br> <div class='social'> <a target='_blank' href='tel:+82-010-xxxx-xxxx'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>010xxxxxxxx</label></div> </a> <a href='mailto:doanhuyendth@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/dthcs'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/dth96_'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/dthcsc/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
 var resumeString = "<img src='images/resumeThumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Doan Huyen.pdf</label></div><a href='assets/CV.pdf' download='CV.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
 var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d202405.34241565317!2d126.97413929999999!3d37.56487609999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca2012d5c39cf%3A0x7e11eca1405bf29b!2sSeoul!5e0!3m2!1sen!2skr!4v1692831696861!5m2!1sen!2skr' class='map'></iframe></div><label class='add'><address>xxx<br>xxx<br>Seoul, Korea</address>";
 function startFunction() {
@@ -72,17 +72,17 @@ function waitAndResponce(inputText) {
     switch (inputText.toLowerCase().trim()) {
         case "intro":
             setTimeout(() => {
-                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Doan Thanh Huyen</a> :) </span><br><br>I am a Computer Science Engineering student at <span class='bold'>University of Seoul 👨🏻‍💻📚</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+                sendTextMessage("Hello there 👋🏻,<br><br>I'm <span class='bold'><a class='alink'>Doan Thanh Huyen</a> :) </span><br><br>Currently a student of Computer Science Engineering at <span class='bold'>University of Seoul 👨🏻‍💻📚</span><br><br> I'm really excited to learn about potential career opportunities, so I'd be delighted to have a conversation about any job openings in the field of Computer Science.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
             }, 2000);
             break;
         case "help":
-            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
+            sendTextMessage("<span class='sk'>Send following keywords to learn more about me...<br><br><span class='bold'>'skills'</span> - to find out about my skills<br><span class='bold'>'resume'</span> - to access my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'contact'</span> - to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span> <br><br> It's my pleasure to provide you with information you're looking for!");
             break;
         case "resume":
             sendTextMessage(resumeString);
             break;
         case "skills":
-            sendTextMessage("<span class='sk'>I am currently pursuing B.Tech degree in Computer Science Engineering.<br><br>I can comfortably write code in following languages :<br><span class='bold'>Java<br>JavaScript<br>Python<br>CSS<br>HTML</span><br><br>I've experiance with following frameworks :<span class='bold'><br>Nodejs<br></span>");
+            sendTextMessage("<span class='sk'>I am currently pursuing B.Tech degree in Computer Science Engineering.<br><br>I can comfortably write code in following languages :<br><span class='bold'>Java<br>JavaScript<br>Python<br>CSS<br>HTML</span><br><br>I have experience working with the following frameworks:<span class='bold'><br>Express - Node.js<br></span>");
             break;
 
         case "education":
@@ -96,13 +96,13 @@ function waitAndResponce(inputText) {
             clearChat();
             break;
         case "about":
-            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https:\/\/www.facebook.com/doanhuyendth/'><span class='bold'>Doan Huyen</a> with ❤️</span>");
+            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!");
             break;
         case "contact":
             sendTextMessage(contactString);
             break;
         case "projects":
-            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/dthcs'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
+            sendTextMessage("Interested in my projects? <br> Welcome to visit my GitHub account.<br><br><div class='social'><a target='_blank' href='https://github.com/dthcs'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
             break;
         case "new":
             sendTextMessage(addressString);
